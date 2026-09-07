c80205d05793a8c1628aac53dd9343456ab5b4d3
Aguirre5 <d49557123@alumnos.itr.edu.ar>
2026-09-07 08:34:35 -0300

    Juan me esclavizaa.

--- Back-End/app.py
+++ Back-End/app.py
@@ -1,26 +1,33 @@
 from extension import db
-from models import ( Rol, Usuario, CicloLectivo, Curso, DocenteCurso, Inscripcion )
 from routes import main_bp, auth_bp, courses_bp
-from flask import Flask, render_template, request, jsonify
-from werkzeug.security import generate_password_hash, check_password_hash
-import json
+from flask import Flask, app
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 FRONT_END_DIR = os.path.join(BASE_DIR, "..", "Front-End")
 
-app = Flask(
-    __name__,
-    template_folder=os.path.join(FRONT_END_DIR, "templates"),
-    static_folder=os.path.join(FRONT_END_DIR, "static"),
-)
-app.secret_key = "aula-virtual-secret"
+def create_app():
+    app = Flask(
+        __name__,
+        template_folder=os.path.join(FRONT_END_DIR, "templates"),
+        static_folder=os.path.join(FRONT_END_DIR, "static"),
+    )
+    app.secret_key = "aula-virtual-secret"
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        "mysql+pymysql://root:@127.0.0.1:3306/aula_virtual")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    
+    app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(courses_bp)
+    
+    return app
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = (
-    "mysql+pymysql://root:@127.0.0.1:3306/aula_virtual"
-)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app)
+app = create_app()
 
-DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
+if __name__ == "__main__":
+    
+    app.run(debug=True)
---
+++ Back-End/routes/__init__.py
@@ -0,0 +1,10 @@
+from .main import main_bp
+from .auth import auth_bp
+from .courses import courses_bp
+
+
+__all__ = [
+    "main_bp",
+    "auth_bp",
+    "courses_bp"
+]
